1c696e963dcdc852b31d60ee8b6f1a2f32dc4339
Adhima Narendra <adhima.narendra@primecash.co.id>
2026-04-08 13:52:19 +0700

    coba terbaru

--- app/src/main/java/com/example/adhimarino/myapplication/MapsActivity3.java
+++ app/src/main/java/com/example/adhimarino/myapplication/MapsActivity3.java
@@ -1,14 +1,13 @@
 package com.example.adhimarino.myapplication;
 
-import android.*;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.FragmentActivity;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -117,123 +116,123 @@ public class MapsActivity3 extends FragmentActivity implements OnMapReadyCallbac
         mMap.moveCamera(CameraUpdateFactory.newLatLng(CurrentLocation));
 
         Giwangan = new LatLng(-7.834420, 110.391708);
-        mMap.addMarker(new MarkerOptions().position(Giwangan).title("Terminal Giwangan").snippet("Jl Imogiri Timur").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_shelter)));
+        mMap.addMarker(new MarkerOptions().position(Giwangan).title("Terminal Giwangan").snippet("Jl Imogiri Timur").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_shelterr)));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(Giwangan));
 
         tegalgendu = new LatLng(-7.826051, 110.391738);
-        mMap.addMarker(new MarkerOptions().position(tegalgendu).title("Tegal Gendu 1").snippet("Jl Tegal Gendu").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_shelte)));
+        mMap.addMarker(new MarkerOptions().position(tegalgendu).title("Tegal Gendu 1").snippet("Jl Tegal Gendu").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_shelter)));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(tegalgendu));
 
         JEC = new LatLng(-7.798488, 110.402895);
-        mMap.addMarker(new MarkerOptions().position(JEC).title("JEC").snippet("Jl Janti").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_shelte)));
+        mMap.addMarker(new MarkerOptions().position(JEC).title("JEC").snippet("Jl Janti").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_shelter)));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(JEC));
 
         SoloJanti = new LatLng(-7.783155, 110.411644);
-        mMap.addMarker(new MarkerOptions().position(SoloJanti).title("Janti Utara").snippet("Jl Solo").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_shelte)));
+        mMap.addMarker(new MarkerOptions().position(SoloJanti).title("Janti Utara").snippet("Jl Solo").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_shelter)));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(SoloJanti));
 
         Transmart  = new LatLng(-7.783204, 110.420169);
-        mMap.addMarker(new MarkerOptions().position(Transmart).title("Transmart").snippet("Jl Solo").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_shelte)));
+        mMap.addMarker(new MarkerOptions().position(Transmart).title("Transmart").snippet("Jl Solo").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_shelter)));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(Transmart));
 
         Maguwo = new LatLng(-7.783326, 110.4303992);
-        mMap.addMarker(new MarkerOptions().position(Maguwo).title("Maguwo").snippet("JL solo").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_shelte)));
+        mMap.addMarker(new MarkerOptions().position(Maguwo).title("Maguwo").snippet("JL solo").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_shelter)));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(Maguwo));
 
         Bandara = new LatLng(-7.784518, 110.43569);
-        mMap.addMarker(new MarkerOptions().position(Bandara).title("Bandara").snippet("Bandara ADISUCIPTO").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_shelter)));
+        mMap.addMarker(new MarkerOptions().position(Bandara).title("Bandara").snippet("Bandara ADISUCIPTO").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_shelterr)));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(Bandara));
 
         Disnaker = new LatLng(-7.769320, 110.431056);
-        mMap.addMarker(new MarkerOptions().position(Disnaker).title("Disnaker").snippet("JL Ringroad Utara").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_shelte)));
+        mMap.addMarker(new MarkerOptions().position(Disnaker).title("Disnaker").snippet("JL Ringroad Utara").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_shelter)));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(Disnaker));
 
         Instiper = new LatLng(-7.764492, 110.423604);
-        mMap.addMarker(new MarkerOptions().position(Instiper).title("Instiper").snippet("JL Ringroad Utara").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_shelte)));
+        mMap.addMarker(new MarkerOptions().position(Instiper).title("Instiper").snippet("JL Ringroad Utara").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_shelter)));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(Instiper));
 
         UPN = new LatLng(-7.760525, 110.407873);
-        mMap.addMarker(new MarkerOptions().position(UPN).title("UPN").snippet("JL. Ringroad Utara").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_shelte)));
+        mMap.addMarker(new MarkerOptions().position(UPN).title("UPN").snippet("JL. Ringroad Utara").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_shelter)));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(UPN));
 
         Hartono = new LatLng(-7.758263, 110.399459);
-        mMap.addMarker(new MarkerOptions().position(Hartono).title("Hartono Mall").snippet("Jl Ringroad Utara").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_shelte)));
+        mMap.addMarker(new MarkerOptions().position(Hartono).title("Hartono Mall").snippet("Jl Ringroad Utara").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_shelter)));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(Hartono));
 
         CondongCatur = new LatLng(-7.756636, 110.395825);
-        mMap.addMarker(new MarkerOptions().position(CondongCatur).title("Terminal Condong Catur").snippet("Terminal Condong Catur").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_shelter)));
+        mMap.addMarker(new MarkerOptions().position(CondongCatur).title("Terminal Condong Catur").snippet("Terminal Condong Catur").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_shelterr)));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(CondongCatur));
 
         Manggung = new LatLng(-7.758188, 110.386465);
-        mMap.addMarker(new MarkerOptions().position(Manggung).title("Manggung").snippet("Jl Ringroad Utara").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_shelte)));
+        mMap.addMarker(new MarkerOptions().position(Manggung).title("Manggung").snippet("Jl Ringroad Utara").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_shelter)));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(Manggung));
 
         Sardjito = new LatLng(-7.768367, 110.374060);
-        mMap.addMarker(new MarkerOptions().position(Sardjito).title("RS Sardjito").snippet("RS Sardjito").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_shelte)));
+        mMap.addMarker(new MarkerOptions().position(Sardjito).title("RS Sardjito").snippet("RS Sardjito").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_shelter)));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(Sardjito));
 
         Kopma = new LatLng(-7.774309, 110.375108);
-        mMap.addMarker(new MarkerOptions().position(Kopma).title("Kopma UGM").snippet("Jl Kaliurang").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_shelte)));
+        mMap.addMarker(new MarkerOptions().position(Kopma).title("Kopma UGM").snippet("Jl Kaliurang").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_shelter)));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(Kopma));
 
         Cik = new LatLng(-7.782318, 110.375077);
-        mMap.addMarker(new MarkerOptions().position(Cik).title("Cik Ditiro").snippet("JL Cik Ditiro").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_shelte)));
+        mMap.addMarker(new MarkerOptions().position(Cik).title("Cik Ditiro").snippet("JL Cik Ditiro").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_shelter)));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(Cik));
 
         smp = new LatLng(-7.78729, 110.375275);
-        mMap.addMarker(new MarkerOptions().position(smp).title("SMPN 5").snippet("SMPN 5 ").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_shelte)));
+        mMap.addMarker(new MarkerOptions().position(smp).title("SMPN 5").snippet("SMPN 5 ").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_shelter)));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(smp));
 
         sudirman = new LatLng(-7.782978, 110.369537);
-        mMap.addMarker(new MarkerOptions().position(sudirman).title("BUMIPUTERA").snippet("Jl Sudirman").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_shelte)));
+        mMap.addMarker(new MarkerOptions().position(sudirman).title("BUMIPUTERA").snippet("Jl Sudirman").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_shelter)));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sudirman));
 
         diponegoro = new LatLng(-7.782860, 110.362559);
-        mMap.addMarker(new MarkerOptions().position(diponegoro).title("Diponegoro").snippet("Jl Diponegoro").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_shelte)));
+        mMap.addMarker(new MarkerOptions().position(diponegoro).title("Diponegoro").snippet("Jl Diponegoro").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_shelter)));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(diponegoro));
 
         samsat = new LatLng(-7.787129, 110.359738);
-        mMap.addMarker(new MarkerOptions().position(samsat).title("Samsat").snippet("Jl Tentara Pelajar").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_shelte)));
+        mMap.addMarker(new MarkerOptions().position(samsat).title("Samsat").snippet("Jl Tentara Pelajar").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_shelter)));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(samsat));
 
         jlagran = new LatLng(-7.789509, 110.360186);
-        mMap.addMarker(new MarkerOptions().position(jlagran).title("Stasiun Tugu").snippet("Jl Aggran").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_shelte)));
+        mMap.addMarker(new MarkerOptions().position(jlagran).title("Stasiun Tugu").snippet("Jl Aggran").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_shelter)));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(jlagran));
 
         malbor1 = new LatLng(-7.790666, 110.366059);
-        mMap.addMarker(new MarkerOptions().position(malbor1).title("Malioboro 1").snippet("Jl Malioboro").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_shelte)));
+        mMap.addMarker(new MarkerOptions().position(malbor1).title("Malioboro 1").snippet("Jl Malioboro").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_shelter)));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(malbor1));
 
         malbor2 = new LatLng(-7.795185, 110.365534);
-        mMap.addMarker(new MarkerOptions().position(malbor2).title("Malioboro 2").snippet("Jl Malioboro").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_shelte)));
+        mMap.addMarker(new MarkerOptions().position(malbor2).title("Malioboro 2").snippet("Jl Malioboro").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_shelter)));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(malbor2));
 
         benteng = new LatLng(-7.799874, 110.365035);
-        mMap.addMarker(new MarkerOptions().position(benteng).title("BENTENG VREDENBURG").snippet("Jl Ahmad Yani").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_shelte)));
+        mMap.addMarker(new MarkerOptions().position(benteng).title("BENTENG VREDENBURG").snippet("Jl Ahmad Yani").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_shelter)));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(benteng));
 
         pku = new LatLng(-7.801243, 110.359725);
-        mMap.addMarker(new MarkerOptions().position(pku).title("KH AHMAD DAHLAN").snippet("jL KH AHMAD DAHLAN").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_shelte)));
+        mMap.addMarker(new MarkerOptions().position(pku).title("KH AHMAD DAHLAN").snippet("jL KH AHMAD DAHLAN").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_shelter)));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(pku));
 
         ngabean = new LatLng(-7.803723, 110.356256);
-        mMap.addMarker(new MarkerOptions().position(ngabean).title("Terminal Ngabean").snippet("Terminal Ngabean").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_shelter)));
+        mMap.addMarker(new MarkerOptions().position(ngabean).title("Terminal Ngabean").snippet("Terminal Ngabean").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_shelterr)));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(ngabean));
 
         jokteng = new LatLng(-7.813219, 110.357347);
-        mMap.addMarker(new MarkerOptions().position(jokteng).title("Jokteng (Pugeran)").snippet("Jl MT Haryono").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_shelte)));
+        mMap.addMarker(new MarkerOptions().position(jokteng).title("Jokteng (Pugeran)").snippet("Jl MT Haryono").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_shelter)));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(jokteng));
 
         sugiyono = new LatLng(-7.814801, 110.370073);
-        mMap.addMarker(new MarkerOptions().position(sugiyono).title("SD Pujokusuman").snippet("Jl Kolonel Sugiyono").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_shelte)));
+        mMap.addMarker(new MarkerOptions().position(sugiyono).title("SD Pujokusuman").snippet("Jl Kolonel Sugiyono").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_shelter)));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sugiyono));
 
         wirosaban = new LatLng(-7.824808, 110.379172);
-        mMap.addMarker(new MarkerOptions().position(wirosaban).title("Wirosaban").snippet("Jl Sorogenen").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_shelte)));
+        mMap.addMarker(new MarkerOptions().position(wirosaban).title("Wirosaban").snippet("Jl Sorogenen").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_shelter)));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(wirosaban));
 
         Giwanganakhir = new LatLng(-7.834420, 110.391708);
-        mMap.addMarker(new MarkerOptions().position(Giwanganakhir).title("Terminal Giwangan").snippet("Jl Imogiri Timur").snippet("Jl Imogiri Timur").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_shelter)));
+        mMap.addMarker(new MarkerOptions().position(Giwanganakhir).title("Terminal Giwangan").snippet("Jl Imogiri Timur").snippet("Jl Imogiri Timur").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_shelterr)));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(Giwanganakhir));
 
         arrayLokasi = new LatLng[]{Giwangan, tegalgendu, JEC, SoloJanti, Transmart, Maguwo, Bandara, Disnaker, Instiper,
